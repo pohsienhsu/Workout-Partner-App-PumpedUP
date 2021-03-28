941bb555525d2e7c35a2e7971d7b0c42e5521342
AndyLiao-32 <tliao32@gatech.edu>
2021-03-28 14:00:30 -0400

    Fetch data unfinished

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -9,6 +9,8 @@ import {
   Modal,
   addons,
 } from "react-native"
+import { Icon } from 'react-native-elements'
+
 import { fetchUser, fetchUserPref, fetchUserProfile, clearData } from '../../redux/actions/index';
 
 import firebase from 'firebase'
@@ -40,7 +42,10 @@ const renderImage = (avatar) => {
 function Home(props) {
   const [user, setUser] = useState(null);
   const [profile, setProfile] = useState({})
+  const [pref, setPref] = useState({})
   const [avatar, setAvatar] = useState(null)
+
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -59,7 +64,8 @@ function Home(props) {
     }
 
     fetchData().then(() => {
-      const { currentUser, profile } = props;
+      const { currentUser, pairingPref, profile } = props;
+      setPref(pairingPref);
       setUser(currentUser);
       setProfile(profile);
       try {
@@ -69,7 +75,7 @@ function Home(props) {
       }
     })
 
-}, [props.currentUser, props.profile, avatar])
+}, [props.currentUser, props.profile, props.pairingPref, avatar])
 
 console.log("###################  HOME PAGE  ####################")
 console.log(profile.pictureURL);
@@ -83,6 +89,9 @@ if (user === null) {
 
 // let avatarURL = avatar ? avatar : require()
 
+const total = 5;
+const MatchPPL = 'Kevin Hart';
+
 return (
   <View style={styles.container}>
     {renderImage(avatar)}
@@ -93,30 +102,56 @@ return (
         style={styles.Button}
         onPress={async () => {
           // Open Navigation menu
-          props.navigation.navigate("Invitation")
+          // props.navigation.navigate("Invitation")
 
-          const currUserPref = await firebase.firestore()
-            .collection("users")
-            .doc(firebase.auth().currentUser.uid)
-            .collection("userPref")
-            .doc(firebase.auth().currentUser.uid)
-            .get()
+          modalVisible ? setModalVisible(false) : setModalVisible(true)
+
+          // const currUserPref = await firebase.firestore()
+          //   .collection("users")
+          //   .doc(firebase.auth().currentUser.uid)
+          //   .collection("userPref")
+          //   .doc(firebase.auth().currentUser.uid)
+          //   .get()
 
           // Get all users
           const users = await firebase.firestore()
             .collection("users")
-            .where('name', '==', 'Kevin Hart')
+            // .where('name', '==', 'Kevin Hart')
             .get();
 
-          const total = 5;
-          
           users.forEach(async user => {
             const info = await firebase.firestore().collection("users").doc(user.id).collection("userProfile").doc(user.id).get();
+            const score = 0;
+
             if (info.exists && user.id != firebase.auth().currentUser.uid) {
+              // BodyPart is an array
+              console.log(info.get("bodyPart"))
+              info.get("bodyPart").forEach(async eachInfo => {
+                if (eachInfo.get("bodyPart").includes(pref.get("bodyPart")[0])) {
+                  score += 2.75;
+                }
+              })
+
+              if (eachInfo.get("experience") == pref.get("experience")) {
+                score += 2.5;
+              }
+
+              if (eachInfo.get("frequency") == pref.get("frequency")) {
+                score += 2.25;
+              }
+
+              if (pref.get("gender")[eachInfo.get("gender")]) {
+                score += 2;
+              }
+
+              if (score == total) {
+                MatchPPL = eachInfo.get("name");
+              }
+
+              // For testing check
               if (info.get("age") == "42") {
                 console.log('User id: ', user.id, ' Data:', info.data());
                 // Load
-
               }
             }
           })
@@ -127,11 +162,67 @@ return (
         {/* <Text style={styles.ButtonText}>+1</Text> */}
       </TouchableOpacity>
 
+      <Modal transparent={true} visible={modalVisible} onRequestClose={() => { setModalVisible(!modalVisible) }}>
+        <View style={{ backgroundColor: '#000000aa', flex: 1 }}>
+          <View style={styles.ModalBox}>
+            <Image
+              style={styles.ModalImage}
+              source={{ uri: data[1].img }}
+            />
+
+            <Text style={styles.ModalName}>{MatchPPL}</Text>
+
+            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+              <Text style={styles.ModalText}>{data[1].intro}</Text>
+            </View>
+
+            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+              <Text style={styles.ModalText}>Gender: {data[1].gender}</Text>
+              <Text style={styles.ModalText}>Age: {data[1].age}</Text>
+              <Text style={styles.ModalText}>Weight: {data[1].weight} lb</Text>
+              <Text style={styles.ModalText}>Hobby: {data[1].hobby}</Text>
+            </View>
+            <View style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginBottom: 20
+            }}>
+              <View style={styles.IconBoxCheck}>
+                <TouchableOpacity
+                  style={styles.IconButton}
+                  onPress={() => {
+                    props.navigation.navigate("PairUp");
+                    setModalVisible(false)
+                  }}
+                >
+                  <Icon name='check' color='green' />
+                </TouchableOpacity>
+              </View>
+
+              <View style={{ paddingLeft: 40 }} />
+
+              <View style={styles.IconBoxClose}>
+                <TouchableOpacity
+                  style={styles.IconButton}
+                    onPress={() => {
+                      setModalVisible(false);
+                    }}
+                >
+                  <Icon name='close' color='red' />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      </Modal>
+
       <View style={{ paddingLeft: 8 }} />
 
       <TouchableOpacity
         style={styles.Button}
         onPress={() => {
+          props.navigation.navigate("Invitation")
           // console.log(modalVisible)
           // if (modalVisible.localeCompare('none') == 0) {
           //   setModalVisible('flex');
@@ -180,6 +271,36 @@ const mapDispatchProps = (dispatch) => bindActionCreators({
   // fetchUserProfile
 }, dispatch)
 export default connect(mapStateToProps, mapDispatchProps)(Home)
+
+const data = [
+  {
+    name: 'Conan O\'Brien',
+    intro: 'If you can really laugh at yourself loud and hard every time you fail,\nPeople will think you\' drunk',
+    img: 'https://tvline.com/wp-content/uploads/2020/11/conan-ending.jpg?',
+    gender: 'Male',
+    age: 57,
+    weight: '190',
+    hobby: 'Self pity'
+  },
+  {
+    name: 'Kevin Hart',
+    intro: 'Tallest and Fastest Man on Earth\nNever mess with me!',
+    img: 'https://www.blackenterprise.com/wp-content/blogs.dir/1/files/2020/05/Kevin-Hart-Headshot-Kevin-Kwan-High-Res--scaled-e1589926838234.jpg',
+    gender: 'Male',
+    age: 40,
+    weight: '150',
+    hobby: 'Cat, Movies, Thug Life'
+  },
+  {
+    name: 'Gordon Ramsay',
+    intro: 'This lamb is so undercooked,\n it\'s following Mary to school!',
+    img: 'https://www.telegraph.co.uk/content/dam/news/2016/09/29/6455882-ramsay-news_trans_NvBQzQNjv4BqbRF8GMdXQ5UNQkWBrq_MOBxo7k3IcFzOpcVpLpEd-fY.jpg',
+    gender: 'Male',
+    age: 54,
+    weight: '200',
+    hobby: 'Cook, Make adults cry'
+  }
+];
 
 const styles = StyleSheet.create({
   container: {
@@ -231,5 +352,58 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     justifyContent: "center",
     flex: 1
-  }
+  },
+  ModalBox: {
+    flex: 1,
+    marginTop: 80,
+    marginBottom: 80,
+    marginLeft: 40,
+    marginRight: 40,
+    borderRadius: 10,
+    backgroundColor: '#EF9C2E',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  ModalImage: {
+    marginTop: 20,
+    height: 160,
+    width: 160,
+    borderRadius: 150,
+    alignSelf: "center"
+  },
+  ModalName: {
+    color: 'black',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  ModalText: {
+    color: 'black',
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginHorizontal: 50
+  },
+  IconBoxCheck: {
+    width: 54,
+    height: 54,
+    backgroundColor: 'green',
+    borderRadius: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  IconBoxClose: {
+    width: 54,
+    height: 54,
+    backgroundColor: 'red',
+    borderRadius: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  IconButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#EF9C2E',
+    borderRadius: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 })
